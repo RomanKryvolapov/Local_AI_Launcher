bc5d16c11639f3ae2c836bb3c254b2609542622c
RomanKryvolapov <37460379+RomanKryvolapov@users.noreply.github.com>
2025-05-14 17:14:55 +0300

    Update

--- llama-cpp-engine/src/main/cpp/llama_cpp/common/build-info.cpp
+++ llama-cpp-engine/src/main/cpp/llama_cpp/common/build-info.cpp
@@ -1,4 +1,4 @@
-int LLAMA_BUILD_NUMBER = 25;
-char const *LLAMA_COMMIT = "67d6d14";
+int LLAMA_BUILD_NUMBER = 29;
+char const *LLAMA_COMMIT = "42273f6";
 char const *LLAMA_COMPILER = "Android (12027248, based on r522817) clang version 18.0.1 (https://android.googlesource.com/toolchain/llvm-project d8003a456d14a3deb8054cdaa529ffbf02d9b262)";
 char const *LLAMA_BUILD_TARGET = "x86_64-w64-windows-gnu";
